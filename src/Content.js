f4fc0cc1580dbd7a75b81735b26a0333786f2440
Raoul1996 <baobaozhou199606@gmail.com>
2017-08-28 18:43:22 +0800

    What: 使用真正的react-redux和redux
    Why:
    
    * 我为什么要这么做？
    
    How:
    
    * 我是怎么做的？这么做会有什么副作用？

--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import ThemSwitch from './ThemSwitch'
 import PropTypes from 'prop-types'
-import {connect} from './react-redux'
+import {connect} from 'react-redux'
 
 class Content extends Component {
   static propTypes = {
--- src/index.js
+++ src/index.js
@@ -5,20 +5,8 @@ import registerServiceWorker from './registerServiceWorker'
 import './App.css'
 import Header from './Header'
 import Content from './Content'
-import {Provider} from './react-redux'
-
-function createStore(reducer) {
-  let state = null
-  const listeners = []
-  const subscribe = (listener) => listeners.push(listener)
-  const getState = () => state
-  const dispatch = action => {
-    state = reducer(state, action)
-    listeners.forEach((listener) => listener())
-  }
-  dispatch({})
-  return {getState, dispatch, subscribe}
-}
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
 
 const themeReducer = (state, action) => {
   if (!state) {
